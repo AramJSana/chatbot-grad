5a98e30c6ccfd851b40f3154dd40510bd3a6f47a
Aram Sana <aramjosuhe@protonmail.com>
2026-05-20 12:55:38 -0500

    fixed api access

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,8 @@ from openai import AzureOpenAI
 
 MODEL = "gpt-4o"
 
+print(os.getenv('DIAL_KEY'))
+
 client = AzureOpenAI(
     api_key         = os.getenv("DIAL_KEY"),
     api_version     = "2025-04-01-preview",
