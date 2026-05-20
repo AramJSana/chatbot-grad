3da824c2969d5e41edfc8b56482255aeaeeb1ee9
Aram Sana <aramjosuhe@protonmail.com>
2026-05-20 12:43:38 -0500

    Fixed and renamed yaml and fixed how the token is accessed

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ from openai import AzureOpenAI
 MODEL = "gpt-4o"
 
 client = AzureOpenAI(
-    api_key         = dbutils.secrets.get(scope="dbrickstoken", key="dialtoken"),
+    api_key         = os.getenv("DIAL_KEY"),
     api_version     = "2025-04-01-preview",
     azure_endpoint  = "https://ai-proxy.lab.epam.com"
 )
